06fb68c7ea4673e6ce16d6c63dc6d12fcc3318a4
Fariz Ali <farizali0404@gmail.com>
2025-12-07 21:06:18 +0700

    merubah sedikit

--- klasifikasi_mobil.py
+++ klasifikasi_mobil.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 import seaborn as sns
